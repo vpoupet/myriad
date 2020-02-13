2da3cefdbe1348c8aba2e39f7292083a55f16008
vpoupet <victor.poupet@gmail.com>
2020-02-13 13:15:03 +0100

    Adjusted the z-index of titles to make text easily selectable

--- slides/themes/myriad/myriad.js
+++ slides/themes/myriad/myriad.js
@@ -5,20 +5,7 @@ function count_as_page (section) {
 }
 
 function toggleAnimation() {
-    for (let sheet of document.styleSheets) {
-        if (sheet.href && sheet.href.endsWith("myriad.css")) {
-            for (let rule of sheet.cssRules) {
-                if (rule.selectorText && rule.selectorText.startsWith("section.") && rule.style['backgroundImage']) {
-                    if (rule.style['backgroundImage'].includes("/animated")) {
-                        rule.style['backgroundImage'] = rule.style['backgroundImage'].replace("/animated", "");
-                    } else {
-                        rule.style['backgroundImage'] = rule.style['backgroundImage'].replace("images/", "images/animated/");
-                    }
-                }
-            }
-            break;
-        }
-    }
+    document.body.classList.toggle("animated");
 }
 
 window.addEventListener(
