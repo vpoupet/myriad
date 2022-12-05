47f834b5fdabd9e045bb3266f5af52b1f72fbc53
vpoupet <victor.poupet@gmail.com>
2022-12-05 09:04:13 +0100

    Changed navigation links
    - links are thinner
    - added toggleNavigation() function
    - footnotes are higher (to be clickable over links)

--- slides/slides.js
+++ slides/slides.js
@@ -483,3 +483,65 @@ function count_as_page(section) {
 }
 
 window.addEventListener("load", process_slides, false);
+
+
+/***********************************
+ * Code for swipe-based navigation *
+ ***********************************/
+
+let touchStartTime = undefined;
+let touchStartX = undefined;
+let touchStartY = undefined;
+const PI = Math.PI;
+const angleEpsilon = .2;
+
+window.addEventListener('touchstart', e => {
+    touchStartX = e.changedTouches[0].screenX;
+    touchStartY = e.changedTouches[0].screenY;
+    touchStartTime = e.timeStamp;
+})
+
+function onSwipe(angle) {
+    if (-angleEpsilon <= angle && angle <= angleEpsilon) {
+        // right swipe
+        console.log("right");
+        next_step();
+    } else if (angle > PI - angleEpsilon || angle < -PI + angleEpsilon) {
+        // left swipe
+        console.log("left");
+        prev_step();
+    } else if (-PI/2 - angleEpsilon <= angle && angle <= -PI/2 + angleEpsilon) {
+        // up swipe
+        console.log("up");
+        prev_slide();
+    } else if (PI/2 - angleEpsilon <= angle && angle <= PI/2 + angleEpsilon) {
+        // down swipe
+        console.log("down");
+        next_slide();
+    }
+}
+
+/**
+ * Toggles the visibility of the navigation buttons (prev/next slide)
+ */
+function toggleNavigation() {
+    const prevLink = document.querySelector('#prev-link');
+    const nextLink = document.querySelector('#next-link');
+    if (prevLink.style.display === 'none') {
+        prevLink.style.display = 'block';
+        nextLink.style.display = 'block';
+    } else {
+        prevLink.style.display = 'none';
+        nextLink.style.display = 'none';
+    }
+}
+
+window.addEventListener('touchend', e => {
+    const touchDeltaX = e.changedTouches[0].screenX - touchStartX;
+    const touchDeltaY = e.changedTouches[0].screenY - touchStartY;
+    const touchDuration = e.timeStamp - touchStartTime;
+    const touchLength = Math.sqrt(touchDeltaX * touchDeltaX + touchDeltaY * touchDeltaY);
+    if (touchDuration < 500 && touchLength > 100) {
+        onSwipe(Math.atan2(touchDeltaY, touchDeltaX));
+    }
+})
